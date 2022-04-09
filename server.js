ad2b5a28d59676fab7dc6d751e0c597cb55676fb
bgatts <59676688+bgatts@users.noreply.github.com>
2022-04-09 15:31:11 -0400

    having an issue with server hosing but maybe working better

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.get('/app/flip/call/tails',(req,res)=>{
 
 
 
-const server = process.env.PORT || 5000
-app.listen(server, ()=> console.log(`listneing on port ${server}...`))
+const port = process.env.port || 5000
+app.listen(port, ()=> console.log(`listneing on port ${port}...`))
 
